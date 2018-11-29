49853916e1b91859d7c85ab6aa6451d822b75c62
jeffreytinnea <jeffrey.tinnea@titanhre.com>
2018-11-28 17:02:55 -0800

    Commit all files

---
+++ JeffreyTinneaOptimizedMath/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace JeffreyTinneaOptimizedMath
+{
+    class Program
+    {
+        // Number to start counting with (inclusive)
+        static readonly int _startNumber = 1;
+        
+        // Number to stop counting with (inclusive)
+        static readonly int _endNumber = 100;
+
+        /// <summary>
+        /// Application which loops through numbers from fixed 1 to 100 and prints a relevant property about each one.
+        /// </summary>
+        /// <param name="args">Not used</param>
+        static void Main(string[] args)
+        {
+            // Loop for 1 through 100 inclusive per requirement
+            for (int i = _startNumber; i <= _endNumber; i++)
+            {
+                // Checks are in order of priority to print: ex/ 'divisible by two and three' takes priority over 'even'.
+                var divisibleMessage =
+                    i % 6 == 0 ? "divisible by two and three." : // A number divisible by 2 and 3 will be divisible by 6 as 2 and 3 are prime factors of 6.
+                    i % 3 == 0 ? "divisible by three." :
+                    i % 2 == 0 ? "even." : "odd.";
+                Console.WriteLine($"The number '{i}' is {divisibleMessage}");
+            }
+        }
+    }
+}
---
+++ JeffreyTinneaThreadSynchronization/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace JeffreyTinneaThreadSynchronization
+{
+    class Program
+    {
+        // Lock object for threads to interact with _currentNumber
+        static readonly object _threadLock = new object();
+
+        // Max number to count up to for the program
+        static readonly int _maxNumber = 100;
+
+        // Current number to be counted. Must lock on _threadLock to maintain thread safety.
+        // Default value is starting value for counting.
+        static int _currentNumber = 1;
+
+        /// <summary>
+        /// Thread method to count and print numbers from starting _currentNumber to _maxNumber
+        /// </summary>
+        /// <param name="validationExpression">A function returning true if this thread should print and increment the current number.
+        /// Evaluation occurs inside the lock so it is safe to reference _currentNumber.</param>
+        static void PrintNumber(Func<bool> validationExpression)
+        {
+            while (_currentNumber <= _maxNumber)
+            {
+                lock (_threadLock)
+                {
+                    // Only print and increment if the validation expression is true. Additionally, we need
+                    // to verify _currentNumber <= _maxNumber again here because the loop's evaluation is outside the lock
+                    if (_currentNumber <= _maxNumber && validationExpression())
+                    {
+                        Console.WriteLine($"Thread {Thread.CurrentThread.Name}: The number is '{_currentNumber++}'");
+                    }
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Main method which starts the threads
+        /// </summary>
+        /// <param name="args">Not used</param>
+        static void Main(string[] args)
+        {
+            var oddsThread = new Thread(() => PrintNumber(() => _currentNumber % 2 == 1)) { Name = "1" };
+            var evensThread = new Thread(() => PrintNumber(() => _currentNumber % 2 == 0)) { Name = "2" };
+
+            oddsThread.Start();
+            evensThread.Start();
+        }
+    }
+}
